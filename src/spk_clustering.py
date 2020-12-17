336e0a0319b4e6ca3ed6894ff33e18c5897fd9c4
Amir Hussein <36240131+AmirHussein96@users.noreply.github.com>
2020-12-17 23:54:46 +0400

    Update spk_clustering.py

--- src/spk_clustering.py
+++ src/spk_clustering.py
@@ -8,7 +8,6 @@ from tqdm import tqdm
 
 
 
-
 import argparse
 parser = argparse.ArgumentParser(description="To train speaker verification network using voxceleb1", add_help=True)
 parser.add_argument("--max_spks", type=int, default=5,help="source data")
@@ -27,7 +26,6 @@ args.embedding_layer = args.embedding_layer.split('/')[-1]
 max_spks = args.max_spks
 mat = []
 seg_clusters = []
-
 for split in range(1,args.total_split+1):
     temp = np.load(args.embedding_folder+'/'+args.data_folder.split('/')[-1]+'_'+args.model_name+'.'+str(split)+'.'+args.embedding_layer+'.npy')
     mat.extend(temp)
@@ -69,21 +67,21 @@ for iter in range(len(np.unique(uttid))):
     if len(idx)<3:
         clusters = np.zeros([len(idx)],dtype=int)            
     else:
-        if len(idx)>=10:  
+        if len(idx)>=max_spks:  
             spks = max_spks
         else:
             spks = int(len(idx)/2)
 
         X = mat[idx,:]
         # print('Shape: ', X.shape)
-        clustering = AgglomerativeClustering(n_clusters=spks,affinity='cosine',linkage='single').fit(X)
+        clustering = AgglomerativeClustering(distance_threshold=0.7, n_clusters=None, affinity='cosine', linkage='average').fit(X)
         clusters = clustering.labels_
         # print(clusters)
 
     seg_clusters.extend(clusters)
 
 # file write
-fid = open(args.data_folder+'/seg2spk','w')
+fid = open(args.data_folder+'/utt2spk','w')
 for iter in range(len(uttid_segments)):
     fid.write('%s %d\n'%(uttid_segments[iter].rstrip(),seg_clusters[iter]))
 fid.close()
